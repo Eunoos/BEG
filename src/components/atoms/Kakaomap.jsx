011ac96985964024c4158d8cdbfbe28459b48983
으누 <dmsdn18@gmail.com>
2023-12-07 21:59:35 +0900

    문화시설 마커 구현/ 데이터 추출 수정중

--- src/components/atoms/Kakaomap.jsx
+++ src/components/atoms/Kakaomap.jsx
@@ -14,35 +14,9 @@ const MarkerStyle = styled.div`
 `
 export default function Kakaomap() {
     const [location, setLocation] = useState({latitude:0,  longitude:0}) ;
+    const [psList, setPsList] = useState([]);
+    // let loMarkers = [];
     
-    let loMarkers = [];
-    // let ps = new kakao.maps.services.Places();
-    
-   
-    // function displayPlaces(places){
-    //     for ( var i=0; i<places.length; i++ ) {
-
-    //         // 마커를 생성하고 지도에 표시
-    //         var marker = addMarker(new kakao.maps.LatLng(places[i].y, places[i].x));
-
-    //         // 마커와 검색결과 항목을 클릭 했을 때
-    //         // 장소정보를 표출하도록 클릭 이벤트를 등록
-    //         (function(marker, place) {
-    //             // kakao.maps.event.addListener(marker, 'click', function() {
-    //             //     displayPlaceInfo(place);
-    //             // });
-    //         })(marker, places[i]);
-    // }
-    // }
-    // function addMarker(position){
-    //     let marker = new kakao.maps.Marker({
-    //         position: position, // 마커의 위치
-    //     });
-    //     marker.setMap(map);
-    //     loMarkers.push(marker)
-        
-    //     return marker;
-    // }
     useMemo(()=>{
         if (navigator.geolocation){
           navigator.geolocation.getCurrentPosition(success, error);
@@ -65,51 +39,63 @@ export default function Kakaomap() {
       },[navigator.geolocation.getCurrentPosition]);
 
     useEffect(()=>{
+      
         const container = document.getElementById('map');
         const options = {
             center: new kakao.maps.LatLng(location.latitude, location.longitude),
-            level: 3
+            level: 5
         };
         const map = new kakao.maps.Map(container, options);
         let markerPosition = new kakao.maps.LatLng(location.latitude, location.longitude);
         let marker = new kakao.maps.Marker({position: markerPosition});
-        let ps = new kakao.maps.services.Places();
+        let ps = new kakao.maps.services.Places(map);
         let infowindow = new kakao.maps.InfoWindow({zindex:1});
-        ps.categorySearch('SC4', placesSearchCB, {useMapBounds:true});
+        
+        ps.categorySearch('CT1', placesSearchCB, {useMapBounds:true});
         function placesSearchCB(data, status, pagination){
           if (status === kakao.maps.services.Status.OK) {
              data.map((e)=>{
               
               displayMarker(e);
-             })
+              
+             }) 
           }
+
          }
         function displayMarker(place) {
           // 마커를 생성하고 지도에 표시합니다
-          const marker = new kakao.maps.Marker({
+          let markers = new kakao.maps.Marker({
               map: map,
               position: new kakao.maps.LatLng(place.y, place.x) 
           });
-          // if(kakao){
-          //   alert('tjd')
-          // }
+          markers.setMap(map);
+          if(place.place_name.length>0){
+            setPsList(...psList, place.place_name);
+          }
+          '<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>';
           // 마커에 클릭이벤트를 등록합니다
-          kakao.maps.event.addListener(marker, 'click', function() {
+          kakao.maps.event.addListener(markers, 'click', function() {
               // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
             infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-            infowindow.open(map, marker);
+            infowindow.open(map, markers);
             
           });
-          marker.setMap(map);
+          
           }
        
         marker.setMap(map);
         
     }, [location]);
-
+    console.log(psList)
   return (
     <>
       <MapContainer id="map" />
+      <div>
+        {/* {psList.length!=0 ?psList.map((v)=>{
+          return <li></li>
+        }):null} */}
+        {/* 데이터추출에러 수정중 */}
+      </div>
     </>
   );
 }
